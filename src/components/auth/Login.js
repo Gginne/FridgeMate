4cb4dd113a828c496fd6aea24faf52c3d6b3d5ff
mayaaio <mayaitkin-ofer@maya.itkin-ofer>
2023-07-04 13:33:14 -0400

    login mantine

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,26 +1,35 @@
-import React, {useRef, useState } from "react"
-import { Form, Button, Card, Alert } from "react-bootstrap"
+import React, { useState } from "react"
 import { useAuth } from "../../contexts/AuthContext"
 import { Link, Navigate, useNavigate} from "react-router-dom"
 import CenteredContainer from "../commons/CenteredContainer"
+import { useForm } from '@mantine/form';
+import { Alert, Stack, Text, TextInput, Button, Card, PasswordInput, Title } from '@mantine/core';
 
 export default function Login() {
   const { currentUser } = useAuth()
   const navigate = useNavigate()
-  const emailRef = useRef()
-  const passwordRef = useRef()
   const { login } = useAuth()
   const [error, setError] = useState("")
   const [loading, setLoading] = useState(false)
 
+  const form = useForm({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+    },
+  });
 
   async function handleSubmit(e) {
     e.preventDefault()
+    form.validate();
 
     try {
-      setError("")
       setLoading(true)
-      await login(emailRef.current.value, passwordRef.current.value)
+      await login(form.values.email, form.values.password)
       navigate("/")
     } catch(e) {
       console.log(e)
@@ -34,28 +43,33 @@ export default function Login() {
   return (
     currentUser === null ? (
       <CenteredContainer>
-        <Card>
-          <Card.Body>
-            <h2 className="text-center mb-4">Log In</h2>
-            {error && <Alert variant="danger">{error}</Alert>}
-            <Form onSubmit={handleSubmit}>
-              <Form.Group id="email">
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email" ref={emailRef} required />
-              </Form.Group>
-              <Form.Group id="password">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" ref={passwordRef} required />
-              </Form.Group>
-              <Button disabled={loading} className="w-100" type="submit">
-                Log In
-              </Button>
-            </Form>
-          </Card.Body>
-        </Card>
-        <div className="w-100 text-center mt-2">
-          Need an account? <Link to="/signup">Sign Up</Link>
-        </div>
+       <Card shadow="sm" withBorder>
+          <Title
+            align="center"
+          >
+            Log in
+          </Title>
+            {error && <Alert my='sm' color="red" variant="filled">{error}</Alert>}
+            <form onSubmit={handleSubmit}>
+              <TextInput 
+                label="Email"
+                placeholder="your@email.com"
+                withAsterisk
+                {...form.getInputProps('email')}
+              />
+               <PasswordInput 
+                mt='sm'
+                placeholder="Password"
+                label="Password"
+                withAsterisk
+                {...form.getInputProps('password')}
+               />
+               <Button  mt="lg"  type="submit">Submit</Button>
+            </form>
+            </Card>
+         <Text mt='sm'>
+            Need an account? <Link to="/signup">Sign Up</Link>
+          </Text>
       </CenteredContainer>
     ) :  <Navigate to={"/"} replace /> 
   )
