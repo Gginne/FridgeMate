b41c20a8a4bd9fcd924192175ba46c6403a84ea0
gginne <gc348@njit.edu>
2023-07-05 18:23:47 -0400

    Spoonacular api hook

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,16 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import useApi from "../hooks/useApi";
 
 export default function Dashboard() {
 
+  const groceryReq = useApi('/food/ingredients/search')
   
+  useEffect(() => {
+    groceryReq.fetchData({query: 'apple'})
+  }, [groceryReq.fetchData])
+
   return (
+
     <div className="">
 
     
