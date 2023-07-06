08c729c55d1ecee8bf7c4a332717f6f4414b3dc6
mayaaio <mayaitkin-ofer@maya.itkin-ofer>
2023-07-06 15:00:47 -0400

    theme

--- src/components/auth/AuthLayout.js
+++ src/components/auth/AuthLayout.js
@@ -1,17 +1,17 @@
 import React from "react"
-import { AppShell, Button, Text, Header, Group, Grid } from '@mantine/core';
+import { AppShell, Grid } from '@mantine/core';
 import { useAuth } from "../../contexts/AuthContext"
-import { Navigate, Outlet, Link} from "react-router-dom"
+import { Navigate, Outlet} from "react-router-dom"
 import Sidebar from "../commons/Sidebar";
 export default function AuthLayout() {
-  const { currentUser, logout } = useAuth()
+  const { currentUser } = useAuth()
 
   return currentUser !== null ? (
     <AppShell
-      
+      navbar={<Sidebar />}
     >
          <Grid>
-          <Grid.Col span={1}><Sidebar /></Grid.Col>
+          <Grid.Col span={1}></Grid.Col>
           <Grid.Col span={10}>
             <Outlet />
           </Grid.Col>
---
+++ src/components/commons/Theme.js
@@ -0,0 +1,21 @@
+export const FridgeMateTheme = {
+    colors: {
+        brand: [
+            "#BED2CB",
+            "#A5C6BB",
+            "#8BBFAD",
+            "#71BBA1",
+            "#37C191",
+            "#3EA17F",
+            "#41886F",
+            "#417462",
+            "#3F6457",
+            "#384C45"
+          ],
+      },
+    colorScheme: 'light',
+    primaryColor: 'brand',
+    primaryShade: 4,
+  };
+
+  
