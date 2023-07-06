60db81351949a450486393e1a52db5fdca6e0489
mayaaio <mayaitkin-ofer@maya.itkin-ofer>
2023-07-06 00:39:57 -0400

    add items to database

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,20 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect }from "react";
+import { Button } from '@mantine/core';
+import AddItemModal from "./dashboard/AddItemModal";
 import useApi from "../hooks/useApi";
 
 export default function Dashboard() {
-
-  const groceryReq = useApi('/food/ingredients/search')
+  const [addItemOpen, setAddItemOpen] = useState(false)
+  // const groceryReq = useApi('/food/ingredients/search')
   
-  useEffect(() => {
-    groceryReq.fetchData({query: 'apple'})
-  }, [groceryReq.fetchData])
+  // useEffect(() => {
+  //   groceryReq.fetchData({query: 'apple'})
+  // }, [groceryReq.fetchData])
+
 
   return (
 
     <div className="">
 
-    
+    <AddItemModal opened={addItemOpen} onClose={() => setAddItemOpen(false)}/>
      <h2>My Fridge</h2>
+     <Button onClick={() => setAddItemOpen(true)}>Add food item</Button>
      
     </div>
   );
