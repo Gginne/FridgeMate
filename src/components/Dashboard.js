537c1b72d3f14f496947b307fd0874e01231174c
mayaaio <mayaitkin-ofer@maya.itkin-ofer>
2023-07-06 11:23:34 -0400

    add sidebar button

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,17 +1,12 @@
-import React, { useState, useEffect }from "react";
-import { Button } from '@mantine/core';
-import AddItemModal from "./dashboard/AddItemModal";
+import React from "react";
 
 export default function Dashboard() {
-  const [addItemOpen, setAddItemOpen] = useState(false)
 
   return (
 
     <div className="">
 
-    <AddItemModal opened={addItemOpen} onClose={() => setAddItemOpen(false)}/>
      <h2>My Fridge</h2>
-     <Button onClick={() => setAddItemOpen(true)}>Add food item</Button>
      
     </div>
   );
--- src/components/commons/Sidebar.js
+++ src/components/commons/Sidebar.js
@@ -1,21 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Card, Group } from '@mantine/core';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse, faPlus, faBowlFood, faRightFromBracket} from "@fortawesome/free-solid-svg-icons";
 import { useAuth } from "../../contexts/AuthContext"
+import AddItemModal from "./../dashboard/AddItemModal";
 export default function Sidebar() {
     const {logout} = useAuth()
+    const [addItemOpen, setAddItemOpen] = useState(false)
   return (
     <Card className='sidebar' shadow="md" >
-        
              
             <Link to="/"  className="sidebar-link" title="">
                 <FontAwesomeIcon icon={faHouse} size="2x" />
                 <span>Home</span>
             </Link>
-    
-      
+
+            <div className="sidebar-link" >
+            <AddItemModal opened={addItemOpen} onClose={() => setAddItemOpen(false)}/>
+                <FontAwesomeIcon icon={faPlus} size="2x" onClick={() => setAddItemOpen(true)}/>
+                <span>Add</span>
+            </div>
+
             <Link to="/"  className="sidebar-link" title="">
                 <FontAwesomeIcon icon={faBowlFood} size="2x" />
                 <span>Recipe</span>
